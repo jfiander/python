4d52c22ac8b3660e4346c1e38200f7729cb158ee
Julian Fiander <julianfiander@gmail.com>
2016-07-03 09:58:45 -0400

    Clear status on start and display status

--- zeroborg.py
+++ zeroborg.py
@@ -5,3 +5,13 @@ sys.path.insert(0, '/home/pi/repos/vendor/zeroborg')
 import ZeroBorg
 ZB = ZeroBorg.ZeroBorg()
 ZB.Init()
+ZB.MotorsOff()
+ZB.ResetEpo()
+
+print "\nCurrent Status:\n"
+print "Motor 1: ", ZB.GetMotor1()
+print "Motor 2: ", ZB.GetMotor2()
+print "Motor 3: ", ZB.GetMotor3()
+print "Motor 4: ", ZB.GetMotor4()
+
+print "EPO:     ", ZB.GetEpo()
