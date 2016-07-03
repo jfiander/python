ca7b9e9941d2192e651923cd41ba5d614c5e8e88
Julian Fiander <julianfiander@gmail.com>
2016-07-03 10:56:39 -0400

    Add comments to identify motors

--- zeroborg.py
+++ zeroborg.py
@@ -11,9 +11,9 @@ ZB.ResetEpo()
 speed = 1
 
 print "\nCurrent Status:\n"
-print "Motor 1: ", ZB.GetMotor1()
-print "Motor 2: ", ZB.GetMotor2()
-print "Motor 3: ", ZB.GetMotor3()
-print "Motor 4: ", ZB.GetMotor4()
+print "Motor 1: ", ZB.GetMotor1() # Front Left
+print "Motor 2: ", ZB.GetMotor2() # Rear Left
+print "Motor 3: ", ZB.GetMotor3() # Front Right
+print "Motor 4: ", ZB.GetMotor4() # Rear Right
 
 print "EPO:     ", ZB.GetEpo()
