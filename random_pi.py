e4de2cd780c5f65edc9433c7ad801d22e46ee164
Julian Fiander <julianfiander@gmail.com>
2017-03-13 20:46:38 -0400

    Add random_pi.py

---
+++ random_pi.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import math
+from fractions import gcd
+from random import randint
+import sys, getopt
+
+def main(argv):
+  iterations = 1000
+  limit      = 1000
+
+  try:
+    opts, args = getopt.getopt(argv, "n:l:")
+  except getopt.GetoptError as e:                                                                                                                 
+    print('Invalid option:', e)
+    sys.exit(1)
+
+  for opt, arg in opts:
+    if opt == "-n":
+      iterations = int(arg)
+    elif opt == "-l":
+      limit = int(arg)
+
+  step     = 0
+  coprimes = 0
+
+  while step < iterations:
+    a = randint(1, limit)
+    b = randint(1, limit)
+    if gcd(a, b) == 1:
+      coprimes += 1
+    step += 1
+
+  result = math.sqrt(6/(coprimes / iterations))
+
+  print(result)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
